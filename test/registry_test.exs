3c44572bd126f0496275e973473bf385af1fb79b
Mostafa Bahri <mostafa@turpal.com>
2022-08-13 00:48:30 +0430

    monitor agents

--- test/registry_test.exs
+++ test/registry_test.exs
@@ -1,12 +1,24 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  test "registry lookup" do
+  setup do
     registry = start_supervised!(KV.Registry)
+    %{registry: registry}
+  end
+
+  test "registry lookup", %{registry: registry} do
     assert KV.Registry.create(registry, "shopping") == :ok
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 end
