b27c2ed70c337897f1f73b74e92b0a9ae92096d1
Mostafa Bahri <mostafa@turpal.com>
2022-08-12 14:19:42 +0430

    agent list

--- test/kv_task_test.exs
+++ test/kv_task_test.exs
@@ -17,12 +17,22 @@ defmodule KV.TaskTest do
   end
 
   describe "Agent" do
-    test "get/put" do
+    test "map read/write" do
       # testing stuff out
       Agent.start_link(fn -> %{} end, name: :kv)
       Agent.update(:kv, fn map -> Map.put(map, :color, :blue) end)
 
       assert :blue == Agent.get(:kv, fn map -> Map.get(map, :color) end)
+    end
+
+    test "list read/write" do
+      Agent.start_link(fn -> [] end, name: :agent)
+
+      Agent.update(:agent, fn list -> ["cool" | list] end)
+
+      assert ["cool"] == Agent.get(:agent, &Function.identity/1)
+
+      assert ["cool"] == Agent.get(:agent, & &1)
     end
   end
 end
