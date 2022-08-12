6428ccc3c99430e8ecf8fb167de4f23788bab72a
Mostafa Bahri <mostafa@turpal.com>
2022-08-12 14:56:03 +0430

    bucket delete

--- test/bucket_test.exs
+++ test/bucket_test.exs
@@ -13,4 +13,12 @@ defmodule BucketTest do
 
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "deletes values by key", %{bucket: bucket} do
+    KV.Bucket.put(bucket, "cheese", 3)
+
+    assert KV.Bucket.delete(bucket, "cheese") == 3
+
+    assert KV.Bucket.get(bucket, "cheese") == nil
+  end
 end
